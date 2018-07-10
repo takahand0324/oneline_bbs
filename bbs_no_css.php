debd80b7f14695849f26daa1877725af9613a645
森野嵩久 <takahand0324@gmail.com>
2018-07-10 12:20:38 +0800

    12:20 コミット

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -1,5 +1,34 @@
 <?php
+
+$nickname = htmlspecialchars($_POST['nickname']);
+$comment =  htmlspecialchars($_POST['comment']);
   // ここにDBに登録する処理を記述する
+$dsn = 'mysql:dbname=oneline_bbs;host=localhost';
+$user = 'root';
+$password='';
+$dbh = new PDO($dsn, $user, $password);
+$dbh ->query('SET NAMES utf8');
+
+//2.SQL文を実行する
+// アンケート自動保存
+
+
+
+
+$sql = 'INSERT INTO `posts`(`nickname`,`comment`)VALUES (?,?)';
+
+// インジェクション
+    $data[] = 'nickname';
+    $data[] = 'comment';
+
+
+$stml = $dbh->prepare($sql);
+$stml->execute($data);
+
+
+
+//3. データベースを切断する
+$dbh = null;
 
 ?>
 <!DOCTYPE html>
