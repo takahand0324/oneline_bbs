737c8f6afdb929e9cd6779773aef11758b271d6f
森野嵩久 <takahand0324@gmail.com>
2018-07-10 20:40:03 +0800

    からうち表示なし、時間見える

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -1,7 +1,6 @@
 <?php
 
-$nickname = htmlspecialchars($_POST['nickname']);
-$comment =  htmlspecialchars($_POST['comment']);
+
   // ここにDBに登録する処理を記述する
 $dsn = 'mysql:dbname=oneline_bbs;host=localhost';
 $user = 'root';
@@ -13,19 +12,28 @@ $dbh ->query('SET NAMES utf8');
 // アンケート自動保存
 
 
+if(!empty($_POST)){
+    //2.SQL文を実行する
+    // WHEREはどういう条件か
+$nickname = htmlspecialchars($_POST['nickname']);
+$comment =  htmlspecialchars($_POST['comment']);
+$date = new DateTime('2017-08-22 23:44:44');
+echo $date->format('Y年m月d日 H時i分s秒');
 
+if (!($nickname == "" || $comment == "") ){
 
 $sql = 'INSERT INTO `posts`(`nickname`,`comment`)VALUES (?,?)';
 
 // インジェクション
-    $data[] = 'nickname';
-    $data[] = 'comment';
+    $data[] = $nickname;
+    $data[] = $comment;
 
 
 $stml = $dbh->prepare($sql);
 $stml->execute($data);
+}
 
-
+}
 
 //3. データベースを切断する
 $dbh = null;
