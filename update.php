6f4bffc49ff1156055d347ac29390159de0811bb
森野嵩久 <takahand0324@gmail.com>
2018-07-13 09:45:45 +0800

    いけさん解説

--- update.php
+++ update.php
@@ -19,11 +19,12 @@
 
 
     //.SQL文を実行する
-    // 
+    // $sql = 'UPDATE `posts` SET `nickname` = "' . $nickname . '", `comment` = "' . $comment . '" WHERE `id` = ' . $id;
     $sql = 'UPDATE `posts` SET `nickname` = ?, `comment` = ? WHERE `id` = ?';
     $data[] = $nickname;
     $data[] = $comment;
     $data[] = $id;
+    // $data = [$nickname, $comment, $id];
     $stmt = $dbh->prepare($sql);
     $stmt->execute($data);
 
@@ -35,3 +36,20 @@
     // リダイレクト(他のページに飛ばす)
     header("Location: bbs.php");
     exit();
+
+
+    // 変数の場合、シングルクォーテーションが文字列として変換される
+    // $var = 'hoge';
+    // var_dump ("$var", '$var');die();
+
+
+    // $hoge = [1,2,3,4,5];
+    // // 配列に値を追加
+    // $hoge[] = 6;
+
+// 配列の値を上書き
+    // $hoge['0'] = 10;
+
+    // $fuga = ['banana' => 'バナナ'];
+    // $fuga[] = 'huga';
+
